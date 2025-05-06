ed08d2fb3c06b80e3a98a36a9a69be5367853fe3
Inon_Moshe_Yosef <inonnn333@gmail.com>
2025-05-06 22:24:31 +0300

    Fix the server call in product and local env

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'http://localhost:5555'
-const API_URL = 'https://shaot-app-server.onrender.com'
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
     baseURL: API_URL,
