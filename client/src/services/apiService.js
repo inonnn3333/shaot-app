b0babad1b3418ea389315faf29592c2e4b9f280e
Inon_Moshe_Yosef <inonnn333@gmail.com>
2025-07-22 00:58:35 +0300

    Debuging

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = import.meta.env?.PROD ? '' : 'http://localhost:1010';
+const API_URL = import.meta.env.PROD ? '' : 'http://localhost:1010';
 
 const api = axios.create({
     baseURL: API_URL,
